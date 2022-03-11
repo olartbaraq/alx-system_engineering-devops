bb395fc1087eeaee11cb6bbe2272aacb5621579d
Olartbaraq <akanbi.ges201@gmail.com>
2022-03-11 21:27:58 +0100

    update tabs

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Puppet script to create a resource (file)
+# Puppet script to create a file
 
 file { '/tmp/school':
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0774',
-  content => 'I love Puppet',
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0774',
+content => 'I love Puppet',
 }
