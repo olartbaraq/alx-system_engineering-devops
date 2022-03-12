adf05061ccb30c888cb0637061db482116ea5033
Olartbaraq <akanbi.ges201@gmail.com>
2022-03-12 21:57:01 +0100

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+# puppet to make changes to SSH client configuration file
+
+
+exec { 'echo':
+path    => 'usr/bin:/bin',
+command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+returns => [0,1],
+}
