f488ac7b1195e88683ae312f06e25e2d2f8dc72b
olart <akanbi.ges201@gmail.com>
2022-04-11 17:17:16 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,18 +2,19 @@
 """ necessary mudules to import"""
 
 if __name__ == "__main__":
+
     import json
     import requests
     import sys
-    
+
     employee_id = int(sys.argv[1])
-    employee_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
-    todos = json.loads(employee_todo.text)
     emp_data = ("https://jsonplaceholder.typicode.com/users/{}".
                 format(employee_id))
     emp_name = requests.get(emp_data)
     names = json.loads(emp_name.text)
     name = names['name']
+    employee_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    todos = json.loads(employee_todo.text)
 
     num_tasks = 0
     total_tasks = 0
