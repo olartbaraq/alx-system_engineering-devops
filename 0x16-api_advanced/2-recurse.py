7defd63dce9384bbe98ac3483f0feb44f15be57c
olart <akanbi.ges201@gmail.com>
2022-04-26 23:02:23 +0100

    recursion

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Get all hot posts from reddit api using the neccessary
+modules
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], count=0, after=''):
+    """using recursive function to list all hot posts ina given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "MyAPI/0.0.1"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+    results = response.json().get("data")
+    after = results.get("after")
+    count = count + results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
