2c79d85644fc6048f732cc630434bc9ff6d6d2bb
olart <akanbi.ges201@gmail.com>
2022-04-26 18:45:33 +0100

    task 1: How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the numbers of total subscribers on a subreddit"""
-    
-    url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
+
+    url = 'https://www.reddit.com/r/' + subreddit + '/about/.json'
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     res = requests.get(url, allow_redirects=False, headers=headers)
     if (res.status_code == 200):
