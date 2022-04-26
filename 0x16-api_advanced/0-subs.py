a54d5b2d367d7d98668341b026d7ee66728c112e
olart <akanbi.ges201@gmail.com>
2022-04-26 21:32:12 +0100

    used format string

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,12 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the numbers of total subscribers on a subreddit"""
 
-    url = 'https://www.reddit.com/r/' + subreddit + '/about/.json'
+    url = 'https://www.reddit.com/r/{}/about/.json'.\
+        format(subreddit)
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     res = requests.get(url, allow_redirects=False, headers=headers)
     if (res.status_code == 200):
-        resp = json.loads(res.text)
+        resp = res.json()
         subscriber_count = resp['data']['subscribers']
         return subscriber_count
     else:
