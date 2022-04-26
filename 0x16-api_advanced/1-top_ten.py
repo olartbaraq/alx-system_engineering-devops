ed12ddd5073c41f6a68d8449ba20f61318152877
olart <akanbi.ges201@gmail.com>
2022-04-26 21:19:15 +0100

    checker whats the prob

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,9 +4,10 @@
 import json
 import requests
 
+
 def top_ten(subreddit):
     """list top 10 hot posts"""
-    
+
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json?limit=10'
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     res = requests.get(url, allow_redirects=False, headers=headers)
