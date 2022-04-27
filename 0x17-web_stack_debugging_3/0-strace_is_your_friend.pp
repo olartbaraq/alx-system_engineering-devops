42255cc45691fbc0a1ba05acf6e2c92d5c714e6d
olart <akanbi.ges201@gmail.com>
2022-04-27 17:54:36 +0100

    fixed puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,7 +3,7 @@
 exec { 'fix-wordpress':
   environment => ['DIR=/var/www/html/wp-settings.php',
                   'OLD=phpp',
-		  'NEW=php'],
+                  'NEW=php'],
   command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
   path        => ['/usr/bin', '/bin'],
   returns     => [0, 1]
